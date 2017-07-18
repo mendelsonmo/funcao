6f36a7d2b3049b6138a9c207e3610aa9ffb2374a
mendelsonmo <mendelson_oliveira@hotmail.com>
2017-07-17 23:55:31 -0300

    Add files via upload

---
+++ iniSis.php
@@ -0,0 +1,23 @@
+<?php
+//DEFINE BANCO DEDADOS
+define(HOST,'localhost');
+define(USER,'root');
+define(PASS,'');
+define(DBSA,'prophp');
+
+//BASE DO SITE
+define(BASE,'http://localhost/cursos/prophp/projeto');
+define(SITENAME,'Projeto Pro PHP');
+define(SITEDESC,'Descrição do site');
+define(SITETAGS,'tags do site');
+
+//DEFINE O SERVIDOR DE E-MAIL
+define(MAILUSER,'');
+define(MAILPASS,'');
+define(MAILPORT,'');
+define(MAILHOST,'');
+
+//MEUS DADOS
+define(ENDERECO,'Rua Emilo de moraes 1207, Soledade / RS. Cep: 99.300-000');
+define(TELEFONE,'(54) 3381.2185');
+?>
---
+++ setSis.php
@@ -0,0 +1,52 @@
+<?php
+/*****************************
+SETA URL DA HOME
+*****************************/
+
+	function setHome(){
+		echo BASE;	
+	}
+	
+/*****************************
+INCLUE ARQUIVOS
+*****************************/
+
+	function setArq($nomeArquivo){
+		if(file_exists($nomeArquivo.'.php')){
+			include($nomeArquivo.'.php');
+		}else{
+			echo 'Erro ao incluir <strong>'.$nomeArquivo.'.php</strong>, arquivo ou caminho não conferem!';	
+		}
+	}
+
+/*****************************
+TRANFORMA STRING EM URL
+*****************************/
+
+	function setUri($string){
+		$a = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ"!@#$%&*()_-+={[}]/?;:.,\\\'<>°ºª';
+		$b = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                 ';	
+		$string = utf8_decode($string);
+		$string = strtr($string, utf8_decode($a), $b);
+		$string = strip_tags(trim($string));
+		$string = str_replace(" ","-",$string);
+		$string = str_replace(array("-----","----","---","--"),"-",$string);
+		return strtolower(utf8_encode($string));
+	}
+/*****************************
+SOMA VISITAS
+*****************************/	
+
+	function setViews($topicoId){
+		$topicoId = mysql_real_escape_string($topicoId);
+		$readArtigo = read('up_posts',"WHERE id = '$topicoId'");
+		
+		foreach($readArtigo as $artigo);
+			$views = $artigo['visitas'];
+			$views = $views +1;
+			$dataViews = array(
+				'visitas' => $views
+			);
+			update('up_posts',$dataViews,"id = '$topicoId'");
+	}	
+?>
